f1fbd8cae8cd91ca9aca02d694f907999c9d7629
vatrimariano <marianovatri@gmail.com>
2021-11-12 10:11:16 -0300

    reqHeader

--- darBaja.php
+++ darBaja.php
@@ -2,34 +2,12 @@
 	include("conexion.php");
 ?>
 
+<?php require 'req/header.php' ?>
+
 <?php
 	include("function.php");
 ?>
 
-
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
-    <title>Document</title>
-    
-</head>
-
-
-<nav class="navbar navbar-light" style="background-color: #e3f2fd;">
-  <a class="navbar-brand" href="#">
-    <img src="https://assurerbroker.com/wp-content/uploads/2020/10/corazon-con-variante-de-linea-de-vida.png" width="30" height="30" class="d-inline-block align-top" alt="">
-    <h6>Ficticia S.A</h6>
-    <div>
-    <a href="index.php">Registrarse</a>
-    <a href="darBaja.php">Clientes Actuales</a>
-    </div>
-  </a>
-</nav>
 
 
 
